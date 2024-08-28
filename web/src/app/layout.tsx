416f263d877a31e996624167eb1e329d744e9b28
Michael Asiedu <michaelasiedu148@gmail.com>
2024-08-28 15:07:17 +0000

    fix: conflict root layout error

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,14 +1,15 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Figtree } from 'next/font/google'
 import { headers } from 'next/headers'
 import { type ReactNode } from 'react'
 import { cookieToInitialState } from 'wagmi'
 
+
 import { getConfig } from '../wagmi'
 import { Providers } from './providers'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Figtree({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Wagmi',
@@ -22,7 +23,7 @@ export default function RootLayout(props: { children: ReactNode }) {
   )
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className}`}>
         <Providers initialState={initialState}>{props.children}</Providers>
       </body>
     </html>
