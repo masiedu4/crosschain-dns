788b7e407d7ce74250d4b80bd62f8e3c0b19240e
Michael Asiedu <michaelasiedu148@gmail.com>
2024-08-28 14:41:03 +0000

    chore:add tailwind + shadcn config

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+	experimental: {
+	  turbo: {
+	    rules: {
+	      '*.svg': {
+		loaders: ['@svgr/webpack'],
+		as: '*.js',
+	      },
+	    },
+	  },
+	},
+      }
