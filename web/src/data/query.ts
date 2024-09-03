aee0bce83988772d0d77b07396a486207d33b398
Michael Asiedu <michaelasiedu148@gmail.com>
2024-09-03 14:23:01 +0000

    add notices data fetch for games

--- web/src/data/query.ts
+++ web/src/data/query.ts
@@ -68,10 +68,6 @@ export async function fetchNotices(): Promise<NoticesResponse | null> {
   }
 }
 
-// fetch leaderboard notice
-
-// fetch 
-
 export async function fetchNoticeByGameId(
   gameId: number
 ): Promise<GameResult | null> {
@@ -96,6 +92,168 @@ export async function fetchNoticeByGameId(
     }
 
     return null; // If no matching notice is found
+  } catch (error) {
+    throw error;
+  }
+}
+
+// fetch leaderboard
+
+export async function fetchLatestLeaderboard(): Promise<any | null> {
+  try {
+    const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+    const result = await fetchNotices();
+    const notices = result?.data.notices.edges.map((edge) => edge.node);
+
+    if (!notices) {
+      return null;
+    }
+
+    let latestLeaderboard = null;
+    let latestTimestamp = 0;
+
+    for (const notice of notices) {
+      const noticeTimestamp = parseInt(notice.input.timestamp);
+      if (noticeTimestamp <= currentTime && noticeTimestamp > latestTimestamp) {
+        try {
+          const payloadJson = JSON.parse(hexToString(notice.payload));
+          if (payloadJson.type === "leaderboard") {
+            latestLeaderboard = payloadJson;
+            latestTimestamp = noticeTimestamp;
+          }
+        } catch (error) {
+          console.error("Error parsing payload:", error);
+        }
+      }
+    }
+
+    return latestLeaderboard;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// fetch most recent current game
+
+export async function fetchLatestStartGame(
+  msgSender: string
+): Promise<any | null> {
+  try {
+    const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+    const result = await fetchNotices();
+    const notices = result?.data.notices.edges.map((edge) => edge.node);
+
+    if (!notices) {
+      return null;
+    }
+
+    let latestStartGame = null;
+    let latestTimestamp = 0;
+
+    for (const notice of notices) {
+      const noticeTimestamp = parseInt(notice.input.timestamp);
+      if (
+        notice.input.msgSender === msgSender &&
+        noticeTimestamp <= currentTime &&
+        noticeTimestamp > latestTimestamp
+      ) {
+        try {
+          const payloadJson = JSON.parse(hexToString(notice.payload));
+          if (payloadJson.type === "start_game") {
+            latestStartGame = payloadJson;
+            latestTimestamp = noticeTimestamp;
+          }
+        } catch (error) {
+          console.error("Error parsing payload:", error);
+        }
+      }
+    }
+
+    return latestStartGame;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// fetch game results for msg_sender
+
+export async function fetchLatestEndGame(
+  msgSender: string
+): Promise<any | null> {
+  try {
+    const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+    const result = await fetchNotices();
+    const notices = result?.data.notices.edges.map((edge) => edge.node);
+
+    if (!notices) {
+      return null;
+    }
+
+    let latestEndGame = null;
+    let latestTimestamp = 0;
+
+    for (const notice of notices) {
+      const noticeTimestamp = parseInt(notice.input.timestamp);
+      if (
+        notice.input.msgSender === msgSender &&
+        noticeTimestamp <= currentTime &&
+        noticeTimestamp > latestTimestamp
+      ) {
+        try {
+          const payloadJson = JSON.parse(hexToString(notice.payload));
+          if (payloadJson.type === "end_game") {
+            latestEndGame = payloadJson;
+            latestTimestamp = noticeTimestamp;
+          }
+        } catch (error) {
+          console.error("Error parsing payload:", error);
+        }
+      }
+    }
+
+    return latestEndGame;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// most recent  game history for msg.sender
+
+export async function fetchLatestGameHistory(
+  msgSender: string
+): Promise<any | null> {
+  try {
+    const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+    const result = await fetchNotices();
+    const notices = result?.data.notices.edges.map((edge) => edge.node);
+
+    if (!notices) {
+      return null;
+    }
+
+    let latestGameHistory = null;
+    let latestTimestamp = 0;
+
+    for (const notice of notices) {
+      const noticeTimestamp = parseInt(notice.input.timestamp);
+      if (
+        notice.input.msgSender === msgSender &&
+        noticeTimestamp <= currentTime &&
+        noticeTimestamp > latestTimestamp
+      ) {
+        try {
+          const payloadJson = JSON.parse(hexToString(notice.payload));
+          if (payloadJson.type === "game_history") {
+            latestGameHistory = payloadJson;
+            latestTimestamp = noticeTimestamp;
+          }
+        } catch (error) {
+          console.error("Error parsing payload:", error);
+        }
+      }
+    }
+
+    return latestGameHistory;
   } catch (error) {
     throw error;
   }
